cb2e7c78f95925bae908d388d61f3adee2008916
Victoria Jury <victoriaehodges@gmail.com>
2024-03-24 09:11:36 +0100

    add more page links to nav array, qbook booking link url

--- index.php
+++ index.php
@@ -21,8 +21,8 @@ include("template.class.php");
 
 //SET SERVER ROOT LOCATIONS
 function urlRoot(){      //returns root url
-	//$url = "http://localhost/choughs-nest-hotel/"; //TEST SERVER
-	$url = "http://choughsnesthotel.co.uk/test-site-2024/";
+	$url = "http://localhost/choughs-nest-hotel/"; //TEST SERVER
+	//$url = "https://choughsnesthotel.co.uk/test-site-2024/";
 	return $url;
 	}
 function serverRoot(){   //returns root dir of host
@@ -62,62 +62,97 @@ $eating_out_script = '';
 /************************************************************/	
 //PAGES ARRAY
 	$pages = array(
-			array(	"pageURL" 		=> "",	
+			array(	"pageURL" 		=> "",
 					"pagetitle"		=> "",
 					"pagedesc"		=> "Chough's Nest Hotel - BOOK DIRECT for your stay in Lynton &amp; Lynmouth. The hotel has stunning sea views in a prime location for exploring North Devon. Family-run Bed and breakfast accommodation. Exmoor Short Break Special offers available. Best Price Guarantee",
 					"keywords"		=> "hotel, bed and breakfast, accommodation, north devon, exmoor, national park, lynton, lynmouth, book direct, best prices",
 					"nav"			=> "Hotel")
-					
-		  , array(	"pageURL" 		=> "rooms",	
+  
+		  , array(	"pageURL" 		=> "accessibility",
+					"pagetitle"		=> "Accessibility",
+					"pagedesc"		=> "Find out more about accessibility at Chough's Nest Hotel. As it is a Victorian building, there are a few things you should be aware of.",
+					"keywords"		=> "accessibility, access statement, getting around, before your visit, ",
+					"nav"			=> "")
+
+		  , array(	"pageURL" 		=> "rooms",
 					"pagetitle"		=> "Rooms - Four Poster, Superior or Classic Double Bedrooms",
 					"pagedesc"		=> "Rooms at Chough's Nest Hotel feature glorious views of the sea and Exmoor National Park. Four-poster suite, Superior bedrooms and Classic double bedrooms",
 					"keywords"		=> "room, accommodation, superior, double, four poster, facilities, hotel, sea views",
-					"nav"			=> "Rooms & Tariffs")		  
+					"nav"			=> "Rooms & Prices")
+
+		  , array(	"pageURL" 		=> "gallery",
+					"pagetitle"		=> "Photo Gallery",
+					"pagedesc"		=> "View images of the hotel rooms, views and surrounding area at Chough's Nest Hotel",
+					"keywords"		=> "images, picutes, photo gallery, photo collection, album, sea views",
+					"nav"			=> "Gallery")
 					
-		  , array(	"pageURL" 		=> "terms",
+		  , array(	"pageURL" 		=> "terms-and-conditions",
 					"pagetitle"		=> "Booking Terms",
 					"pagedesc"		=> "Terms and Conditions with Access statement, cancellation and booking policy.",
 					"keywords"		=> "booking policy, terms, conditions, access statement, cancellation, pets, smoking, privacy policy, check-in, check-out",
-					"nav"			=> "")		
+					"nav"			=> "")
 
 		
-		  , array(	"pageURL" 		=> "dining",	
+		  , array(	"pageURL" 		=> "dining",
 					"pagetitle"		=> "Dinner at Chough's Nest",
-					"pagedesc"		=> "Prices and tariffs for ".date('Y').", including Exmoor Break Special offers and reduced rates for booking direct online.",
-					"keywords"		=> "tariff, prices, rates, value, book direct,",
-					"nav"			=> "Dinner at Chough's")	
+					"pagedesc"		=> "Dining at Chough's Nest Hotel",
+					"keywords"		=> "dinner, food, dining, menu, eating",
+					"nav"			=> "Dinner at Chough's")
+
+		  , array(	"pageURL" 		=> "breakfast-menu",
+					"pagetitle"		=> "Breakfast Menu",
+					"pagedesc"		=> "What's on offer for breakfast",
+					"keywords"		=> "breakfast, menu, eating",
+					"nav"			=> "")
 					
-		  , array(	"pageURL" 		=> "where-to-eat",	
+		  , array(	"pageURL" 		=> "where-to-eat-in-Lynton-and-Lynmouth",	
 					"pagetitle"		=> "Eating Out - Local Restaurant Guide",
 					"pagedesc"		=> "A list of restaurants, cafes and bars in Lynton and Lynmouth, serving fresh food all year round.",
 					"keywords"		=> "restaurant, eating, dining, cafes, lynton, lynmouth",
-					"nav"			=> "Eating Out")					
+					"nav"			=> "Eating Out")
 
-					
-		  , array(	"pageURL" 		=> "things-to-see-and-do",	
-					"pagetitle"		=> "Lynton &amp; Lynmouth",
-					"pagedesc"		=> "The local area around Chough's Nest Hotel is called Little Switzerland. Home to the Lynton &amp; Lynmouth Cliff Railway, Watersmeet and Valley of Rocks as well as other attractions.",
-					"keywords"		=> "attractions, cliff railway, valley of rocks, watersmeet, lynton, lynmouth, north devon, exmoor national park",
-					"nav"			=> "Things To See &amp; Do")
-				
-		  , array(	"pageURL" 		=> "local-area",	
-					"pagetitle"		=> "Lynton &amp; Lynmouth",
-					"pagedesc"		=> "The local area around Chough's Nest Hotel is called Little Switzerland. Home to the Lynton &amp; Lynmouth Cliff Railway, Watersmeet and Valley of Rocks as well as other attractions.",
+		  , array(	"pageURL" 		=> "a-guide-to-Lynton-and-Lynmouth",
+					"pagetitle"		=> "A Guide to Lynton &amp; Lynmouth",
+					"pagedesc"		=> "The local area around Chough's Nest Hotel is called Little Switzerland.",
 					"keywords"		=> "attractions, cliff railway, valley of rocks, watersmeet, lynton, lynmouth, north devon, exmoor national park",
 					"nav"			=> "")
 					
-		  , array(	"pageURL" 		=> "contact",	
-					"pagetitle"		=> "Contact - Location &amp; Map",
-					"pagedesc"		=> "Send a message, view the hotel's location map and directions. How to get to Lynton &amp; Lynmouth.",
-					"keywords"		=> "contact, message, address, location, directions, map, lynton, lynmouth",
+		  , array(	"pageURL" 		=> "local-attractions-in-Lynton-and-Lynmouth",
+					"pagetitle"		=> "Local Attractions in Lynton &amp; Lynmouth",
+					"pagedesc"		=> "Lynton &amp; Lynmouth is home to the Lynton &amp; Lynmouth Cliff Railway, Watersmeet and Valley of Rocks as well as other attractions.",
+					"keywords"		=> "attractions, cliff railway, valley of rocks, watersmeet, lynton, lynmouth",
+					"nav"			=> "")
+
+		  , array(	"pageURL" 		=> "local-walks-around-Lynton-and-Lynmouth",
+					"pagetitle"		=> "Local Walks around Lynton &amp; Lynmouth",
+					"pagedesc"		=> "Lynton &amp; Lynmouth is a walker's paradise. There are many paths and trails to explore. Chough's Nest Hotel is located on the South West Coast Path.",
+					"keywords"		=> "walking, hiking, trails, paths, SWCP, lynton, lynmouth",
+					"nav"			=> "")
+
+		  , array(	"pageURL" 		=> "things-to-see-and-do",
+					"pagetitle"		=> "Things to see and do in North Devon &amp; Exmoor",
+					"pagedesc"		=> "Information on other attractions near Chough's Nest Hotel in North Devon and Exmoor National Park.",
+					"keywords"		=> "attractions, north devon, exmoor national park",
+					"nav"			=> "Things To See &amp; Do")
+
+		  , array(	"pageURL" 		=> "choughs",	
+					"pagetitle"		=> "What are Chough's?",
+					"pagedesc"		=> "Information on other attractions near Chough's Nest Hotel in North Devon and Exmoor National Park.",
+					"keywords"		=> "attractions, cliff railway, valley of rocks, watersmeet, lynton, lynmouth, north devon, exmoor national park",
+					"nav"			=> "Things To See &amp; Do")
+				
+		  , array(	"pageURL" 		=> "contact",
+					"pagetitle"		=> "Contact Us - How to Get Here",
+					"pagedesc"		=> "Contact details for Chough's Nest Hotel. View the hotel's location map and directions. How to get to Lynton &amp; Lynmouth and parking information",
+					"keywords"		=> "contact, message, address, location, directions, map, lynton, lynmouth, parking",
 					"nav"			=> "Contact")
 					
-		  , array(	"pageURL" 		=> "thanks",	
-							"pagetitle"		=> "Thank You",
-							"keywords"		=> "",
-							"nav"			=> "")	
+		  , array(	"pageURL" 		=> "thanks",
+					"pagetitle"		=> "Thank You",
+					"keywords"		=> "",
+					"nav"			=> "")	
 
-		  , array(	"pageURL" 		=> "error",	
+		  , array(	"pageURL" 		=> "error",
 					"pagetitle"		=> "Error",
 					"keywords"		=> "",
 					"nav"			=> "")						
@@ -200,7 +235,7 @@ $from_date = date('Y-m-d');
 $to_date = date('Y-m-d',strtotime('+1 day'));
 
 $booking_bar = <<<HTMLBLOCK
-	<div id="booking-bar" class="w3-row booking-form iris-bg">
+	<div id="booking-bar" class="w3-row w3-center booking-form iris-bg">
 		<form  action="#" method="post" id="bookingform" autocomplete="off" target="_blank">
 			<label class="floral-white-text">BOOKING</label>
 			<div class="booking-form-inputs">
@@ -216,6 +251,8 @@ $booking_bar = <<<HTMLBLOCK
 	</div>
 HTMLBLOCK;
 
+$qbook_href ='href="https://web-bookings.hotels.uk.com/#/booking/4541/items/availability?k=YS1KvPZvRfrRZlPNVAOaiWYVt" target="_blank"';
+
 $tags = array (
 	
 		"URL"				=> 	urlRoot(),
@@ -226,6 +263,7 @@ $tags = array (
 		"keywords"			=>	$page_keywords,
 		"navigation"		=>	$pagesNav,
 		"booking_bar"		=>	$booking_bar,
+		"qbook_href"		=>	$qbook_href,
 		
 		"tariff_block" 		=>  $tariff_block,
 		"children_12_16_tariff" => $children_12_16_tariff,
