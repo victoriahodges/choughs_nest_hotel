ad7e9d76ab784b3869cb462c03ccb0d29b18639a
Victoria Jury <victoriaehodges@gmail.com>
2024-04-12 08:29:46 +0200

    change from google to openstreetmaps

--- js/map_script.js
+++ js/map_script.js
@@ -1,49 +1,29 @@
-      function choughsNestMap() {
-		const position = { lat: 51.232157, lng: -3.837104 };
+var map = L.map('map_canvas').setView([51.232157, -3.837104], 13);
 
-		var styles = [{
-			featureType: "poi.business",
-			stylers: [{ visibility: "off" }]
-			}];
+var contentString = '<div id="content_map" >'+
+	'<img class="logo" src="images/logo/logo_iris.png" style="width: 200px;" alt="Chough\'s Nest Hotel">'+
+	'<div id="bodyContent">'+
+	'<p>North Walk<br />' +
+	'Lynton<br />' +
+	'Devon<br />' +
+	'EX35 6HJ</p>' +
+	'</div>'+
+	'</div>';
 
-		var myOptions = {
-			center: position,
-			zoom: 12,
-			disableDefaultUI: true,
-			panControl: false,
-			zoomControl: true,
-			scaleControl: true,
-			mapTypeControl: true,
-			styles: styles,
-          	mapTypeId: google.maps.MapTypeId.ROADMAP,
-        };
-		
-		
-        var map = new google.maps.Map(document.getElementById("map_canvas"), myOptions);
-				
-		 var contentString = '<div id="content_map" >'+
-		'<h3 id="firstHeading" class="firstHeading"><img class="logo" src="images/logo/logo_iris.png" style="width: 200px;" alt="Chough\'s Nest Hotel"></h3>'+
-		'<div id="bodyContent">'+
-		'<p>North Walk<br />' +
-		'Lynton<br />' +
-		'Devon<br />' +
-		'EX35 6HJ</p>' +
-		'</div>'+
-		'</div>';
+var marker = L.icon({
+	iconUrl: 'https://www.choughsnesthotel.co.uk/images/location.png',
+	// shadowUrl: 'leaf-shadow.png',
 
-		var infowindow = new google.maps.InfoWindow({
-			content: contentString
-		});
-		 
-		const marker = new google.maps.Marker({
-			map: map,
-			position: position,
-			title: "Chough\'s Nest Hotel, Lynton",
-			icon: "https://www.choughsnesthotel.co.uk/images/location.png"
-		});
-		 
-		google.maps.event.addListener(marker, 'click', function() {
-		  infowindow.open(map,marker);
-		});
+	iconSize:     [64, 64], // size of the icon
+	// shadowSize:   [50, 64], // size of the shadow
+	iconAnchor:   [32, 64], // point of the icon which will correspond to marker's location
+	// shadowAnchor: [4, 62],  // the same for the shadow
+	popupAnchor:  [1, -64] // point from which the popup should open relative to the iconAnchor
+});
 
-		}
+L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+	attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+}).addTo(map);
+
+L.marker([51.232157, -3.837104],{icon: marker}).addTo(map)
+	.bindPopup(contentString);
